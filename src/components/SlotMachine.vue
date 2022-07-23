ba79abdcaebf73f6f0388b966d259b1927138ddb
Seyyed Mojtaba Rezvani <s_mojtaba_rez@yahoo.com>
2022-07-23 02:45:37 -0400

    Bootstrap added to project

--- src/components/SlotMachine.vue
+++ src/components/SlotMachine.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <div class="row">
+      <div class="col-md-6">
+        <table class="table">
+          <tbody>
+            <tr>
+              <td>Cherry</td>
+              <td>Cherry</td>
+              <td>Cherry</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-primary">Primary</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
